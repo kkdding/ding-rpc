f1e39fba26716a1a20d5e68e6694c082f07625c4
wmding <2386585789@qq.com>
2024-09-16 21:50:34 +0800

    搭建本地注册服务器

--- example-provider/src/main/java/com/ding/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/ding/example/provider/EasyProviderExample.java
@@ -1,6 +1,8 @@
 package com.ding.example.provider;
 
+import com.ding.dingrpc.registry.LocalRegistry;
 import com.ding.dingrpc.server.VertxHttpServer;
+import com.ding.example.common.service.UserService;
 
 /**
  * 简易服务提供者示例
@@ -8,6 +10,9 @@ import com.ding.dingrpc.server.VertxHttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
         // 提供服务
         VertxHttpServer vertxHttpServer = new VertxHttpServer();
         vertxHttpServer.start(50000);
